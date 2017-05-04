3d945c3fe4cf1968997d4faffc8169f1b37fa2fa
lingduzero <bz63@njit.edu>
2017-05-04 15:35:21 -0400

    init

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    {title:'javascript: the good parts'},
+    {title:'think in java'},
+    {title:'playboy'},
+    {title:'nba how to be a champion'},
+  ]
+}
